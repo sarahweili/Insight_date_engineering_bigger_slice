4386dbd71877b2871b78d0c06a2b00fd1f3b06cb
liwei6 <li.wei6@husky.neu.edu>
2020-10-01 18:36:45 -0400

    Update join_yelp.py

--- src/spark/join_yelp.py
+++ src/spark/join_yelp.py
@@ -1,5 +1,5 @@
 from pyspark.sql import SparkSession, Row
-from pyspark.sql import functions as F
+from pyspark.sql.functions import col, sum, min, first, size, split, lower, udf, rank, regexp_replace, monotonically_increasing_id, explode
 from pyspark.sql.window import Window
 from math import radians, cos, sin, asin, sqrt
 
@@ -39,14 +39,17 @@ withColumnRenamed('postal_code','zip_code').\
 withColumnRenamed('latitude','sg_latitude').\
 withColumnRenamed('longitude','sg_longitude')
 
-df_poi_location = df_poi_location.withColumn('sg_latitude', F.col('sg_latitude').cast("float")).\
-withColumn('sg_longitude', F.col('sg_longitude').cast("float"))
+df_poi_location = df_poi_location.withColumn('sg_latitude', col('sg_latitude').cast("float")).\
+withColumn('sg_longitude', col('sg_longitude').cast("float"))
+
+df_yelp_location = df_yelp_location.withColumn('latitude', col('latitude').cast("float")).\
+withColumn('longitude', col('longitude').cast("float"))
 
 ## join poi and yelp datasets on state and zip_code
 df_join = df_poi_location.join(df_yelp_location, on = ['state','zip_code'], how='inner')
 
 ## filter out null values in yelp dataset
-df_join = df_join.where(F.col("longitude").isNotNull()).where(F.col("latitude").isNotNull())
+df_join = df_join.where(col("longitude").isNotNull()).where(col("latitude").isNotNull())
 
 ## define the function to calculate distance between two geo locations
 def get_distance(longit_a, latit_a, longit_b, latit_b):
@@ -59,68 +62,89 @@ def get_distance(longit_a, latit_a, longit_b, latit_b):
     distance = central_angle * radius
     return abs(round(distance, 2))
 
-udf_get_distance = F.udf(get_distance)
+udf_get_distance = udf(get_distance)
 
 ## add distance column
 df_distance = df_join.withColumn('distance', udf_get_distance(df_join.sg_longitude, df_join.sg_latitude, df_join.longitude, df_join.latitude))
 
-df_distance = df_distance.withColumn('distance', F.col('distance').cast("float"))
+df_distance = df_distance.withColumn('distance', col('distance').cast("float"))
 
 ## get first 50 match records for each poi
 w = Window.partitionBy(df_distance.safegraph_place_id).orderBy(df_distance.distance)
 
-df_distance = df_distance.withColumn("d_rank",F.rank().over(w))
+df_distance = df_distance.withColumn("d_rank",rank().over(w))
 
 df_sl = df_distance.where(df_distance.d_rank <=50)
 
 ## match location names
-df_names = df_sl.where(F.col('location_name').isNotNull()).where(F.col('alias').isNotNull())
+## standardize strings
+## not null
+df_names = df_sl.where(col('location_name').isNotNull()).where(col('alias').isNotNull())
 
-df_names = df_names.withColumn('sg_name', F.split(F.lower(F.col('location_name')), ' ')).withColumn('yelp_name', F.split(F.lower(F.col('alias')), '-'))
+## lowercase
+df_names = df_names.withColumn('sg_name_temp', lower(col('location_name'))).withColumn('yelp_name_temp', lower(col('alias')))
 
-df_temp = df_names.withColumn("id_new", F.monotonically_increasing_id()).withColumn('word', F.explode('sg_name'))
+## remove ' from names
+df_names = df_names.withColumn('sg_name_temp2', regexp_replace(col('sg_name_temp'), "'",'')).withColumn('yelp_name_temp2', regexp_replace(col('yelp_name_temp'), "'",'')).drop('sg_name_temp', 'yelp_name_temp')
 
-word_match = F.udf(lambda a, b: 0 if a in b else 1)
+## split the string
+df_names = df_names.withColumn('sg_name', split(col('sg_name_temp2'), ' ')).withColumn('yelp_name', split(col('yelp_name_temp2'), '-')).drop('sg_name_temp2', 'yelp_name_temp2')
+
+df_names = df_names.withColumn('name_length', size(col('sg_name')))
+
+## match the names
+df_temp = df_names.withColumn("id_new", monotonically_increasing_id()).withColumn('word', explode('sg_name'))
+
+word_match = udf(lambda a, b: 0 if a in b else 1)
 
 df_temp = df_temp.withColumn('match', word_match('word','yelp_name'))
 
-df_temp = df_temp.withColumn('match', F.col('match').cast("integer"))
+df_temp = df_temp.withColumn('match', col('match').cast("integer"))
 
-df_match = df_temp.groupBy('id_new').agg(F.sum("match").alias('unique_count')\
-        ,F.first("safegraph_place_id").alias('sg_id')\
-        ,F.first('location_name').alias('sg_name')\
-        ,F.first('id').alias('yelp_id')\
-        ,F.first('alias').alias('yelp_name')\
-        ,F.first('distance').alias('distance')\
-        ,F.first("d_rank").alias('d_rank'))\
+df_match = df_temp.groupBy('id_new').agg(sum("match").alias('unique_count')\
+        ,first("safegraph_place_id").alias('sg_id')\
+        ,first('location_name').alias('sg_name')\
+        ,first('id').alias('yelp_id')\
+        ,first('alias').alias('yelp_name')\
+        ,first('distance').alias('distance')\
+        ,first("d_rank").alias('d_rank')\
+        ,first('name_length').alias('name_length'))\
         .orderBy('id_new', ascending=True)
+
+get_unique_pct = udf(lambda a, b: round(a/b, 2))
+
+df_match = df_match.withColumn('match_p', get_unique_pct('unique_count', 'name_length'))
 
 ## find the best match
 w1 = Window.partitionBy('sg_id')
 
-result = df_match.withColumn('max_match', F.min('unique_count').over(w1))\
-    .where(F.col('unique_count') == F.col('max_match'))\
-    .withColumn('min_distance', F.min('distance').over(w1))\
-    .where(F.col('distance') == F.col('min_distance'))
+result = df_match.withColumn('best_match', min('match_p').over(w1))\
+    .where(col('match_p') == col('best_match'))\
+    .withColumn('min_distance', min('distance').over(w1))\
+    .where(col('distance') == col('min_distance'))
 
 ## test the result
-#n = result.count()
-#print('total: ', n)
+n = result.count()
+print('total: ', n)
 
-#result.sample(True, 0.01).show(100, False)
-
+test = result.sample(True, 0.001, 123)
 
 ## write to postgresql
-#df_match.limit(100).write\
-#        .format('jdbc')\
-#        .mode('overwrite')\
-#        .option('url', 'jdbc:postgresql://10.0.0.10:5432/poi_db')\
-#        .option('dbtable', 'test1')\
-#        .option('user','postgres')\
-#        .option('password', 'postgres')\
-#        .option('driver','org.postgresql.Driver')\
-#        .save()
+test.write\
+        .format('jdbc')\
+        .mode('overwrite')\
+        .option('url', 'jdbc:postgresql://10.0.0.10:5432/poi_db')\
+        .option('dbtable', 'test')\
+        .option('user','postgres')\
+        .option('password', 'postgres')\
+        .option('driver','org.postgresql.Driver')\
+        .save()
 
 ## read from postgresql
-#df_test = spark.read.format("jdbc").option("url", "jdbc:postgresql://10.0.0.10:5432/poi_db").option("dbtable", "test1").option("user", "postgres").option('password','postgres').option("driver", "org.postgresql.Driver").load()
-
+#print_test = spark.read.format("jdbc")\
+#        .option("url", "jdbc:postgresql://10.0.0.10:5432/poi_db")\
+#        .option("dbtable", "test")\
+#        .option("user", "postgres")\
+#        .option('password','postgres')\
+#        .option("driver", "org.postgresql.Driver")\
+#        .load()
